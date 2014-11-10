b8be0eecd5a363b26c1ddf218ab69e1516da4b8f
farooq-knr <farooq.knr@gmail.com>
2014-11-10 18:28:54 +0530

    Main.java
    update the file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main extends HttpServlet {
 
   private void showHome(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
+    resp.getWriter().print("Hello from Java12345!");
   }
 
   private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
