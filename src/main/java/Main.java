2c60be0bb84ec41e527ba154b1d358918f6d4c1e
janieltec <fjo@cin.ufpe.br>
2015-07-20 19:27:33 -0300

    UPDATE  01 -shalton o testador

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+            attributes.put("message", "Hello World! - SHALTON O TESTADOR!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
