78cebeaf59ad0a0a7954cb248fa7d50ab81d98e2
janieltec <fjo@cin.ufpe.br>
2015-07-20 19:29:44 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello World -  - SHALTON O TESTADOR!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World! - SHALTON O TESTADOR!");
+            attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
