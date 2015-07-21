ab5d5fe0267def812c72963210f06db25076e5b3
janieltec <fjo@cin.ufpe.br>
2015-07-21 17:52:21 -0300

    Shalton o Testador!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World -  - SHALTON O TESTADOR!");
+    get("/hello", (req, res) -> "Hello World -  - SHALTON 04!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
