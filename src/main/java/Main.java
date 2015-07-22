69100c5f2284f3e8c39f55680f6f8ae04baa3ec2
janieltec <fjo@cin.ufpe.br>
2015-07-22 02:26:49 -0300

    Shalton 11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World -  - SHALTON 10!");
+    get("/hello", (req, res) -> "Hello World -  - SHALTON 11!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
