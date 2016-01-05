5efe33f6215488131860f7e5b4161e1189db3861
Satoshi Kurokawa <tie303802@TISNT.TIS.CO.JP>
2016-01-05 20:30:32 +0900

    Awesome Messagegit stgit st

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello World for Git!!!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
