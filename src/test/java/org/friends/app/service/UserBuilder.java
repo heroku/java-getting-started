80f8fecd5b4fcacb5794e834334cc466cea9b48d
Abdel TAMDITI <tamditi@gmail.com>
2016-02-25 16:48:38 +0100

    test email AMDM

--- src/test/java/org/friends/app/service/UserBuilder.java
+++ src/test/java/org/friends/app/service/UserBuilder.java
@@ -3,11 +3,17 @@ package org.friends.app.service;
 import org.friends.app.model.User;
 
 public class UserBuilder {
+
 	public static UserBuilder unUser() {
 		return new UserBuilder();
 	}
 
 	public User build() {
-		return new User("email@amdm.fr", "mdp");
+		return new User();
 	}
+	
+	public User build(String email, String pwd) {
+		return new User(email, pwd);
+	}
+	
 }
