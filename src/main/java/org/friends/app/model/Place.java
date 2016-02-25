75b328a7b4f7486c7e79cef336a250c8d57f5a53
Abdel TAMDITI <tamditi@gmail.com>
2016-02-25 11:14:23 +0100

    Ajout occupationDate, occupiedBy et modif de number en placeNumber

--- src/main/java/org/friends/app/model/Place.java
+++ src/main/java/org/friends/app/model/Place.java
@@ -1,20 +1,63 @@
 package org.friends.app.model;
 
+import java.util.Date;
+
 public class Place {
 	
+	private Integer placeNumber;
+	private Integer occupiedBy;
+	private Date occupationDate;
 	private boolean free;
-	private int number;
-
+	
 	public Place(int n, boolean f) {
-		number = n;
+		placeNumber = n;
 		free = f;
+	}	
+	
+	/**
+	 * @return the placeNumber
+	 */
+	public Integer getPlaceNumber() {
+		return placeNumber;
 	}
 	
-	public int getNumber() {
-		return number;
+	/**
+	 * @param placeNumber the placeNumber to set
+	 */
+	public void setPlaceNumber(Integer placeNumber) {
+		this.placeNumber = placeNumber;
 	}
 	
+	/**
+	 * @return the occupiedBy
+	 */
+	public Integer getOccupiedBy() {
+		return occupiedBy;
+	}
+	
+	/**
+	 * @param occupiedBy the occupiedBy to set
+	 */
+	public void setOccupiedBy(Integer occupiedBy) {
+		this.occupiedBy = occupiedBy;
+	}
+	
+	/**
+	 * @return the occupationDate
+	 */
+	public Date getOccupationDate() {
+		return occupationDate;
+	}
+	
+	/**
+	 * @param occupationDate the occupationDate to set
+	 */
+	public void setOccupationDate(Date occupationDate) {
+		this.occupationDate = occupationDate;
+	}
+
 	public boolean isFree() {
 		return free;
 	}
+	
 }
