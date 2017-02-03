fa06f60d9a9a57bb358b8dfa2a142c56769f5374
Joe Kutner <jpkutner@gmail.com>
2017-02-03 10:05:11 -0600

    Cleaned up import statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,16 @@
-import java.sql.*;
-import java.util.HashMap;
+import com.heroku.sdk.jdbc.DatabaseUrl;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import static spark.Spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
-import spark.ModelAndView;
-import static spark.Spark.get;
-
-import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
 
