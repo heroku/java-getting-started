68e8ed1ea3be50ff85afde142b2247b63edbc664
Nart Seine <nart.seine@gmail.com>
2018-03-01 11:32:59 +1100

    Added unit tests

---
+++ src/main/java/com/example/Utility.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Utility {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int mul(int x, int y) {
+        return x * y;
+    }
+}
---
+++ src/test/java/com/example/UtilityTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilityTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void mul() {
+    }
+
+    @Test
+    public void sub() {
+    }
+
+    @Test
+    public void div() {
+    }
+}
