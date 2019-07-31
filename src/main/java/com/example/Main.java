cc52a9e2a4519555c5cb5fb8be2f4d48e4fc1e15
Ryan Brown <ryan.brown@barre3.com>
2019-07-31 10:11:46 -0700

    Demo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,6 +16,10 @@
 
 package com.example;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,6 +55,14 @@ public class Main {
   @RequestMapping("/")
   String index() {
     return "index";
+  }
+
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+    RelativisticModel.select();
+    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+    model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+    return "hello";
   }
 
   @RequestMapping("/db")
