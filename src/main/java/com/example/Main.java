c58000b5153d225d97f3770fd5450e9bc734ccbe
AnkitG-Sudo <60222965+AnkitG-Sudo@users.noreply.github.com>
2020-03-04 11:50:29 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2002-2014 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package com.example;
 
@@ -38,51 +38,57 @@ import java.util.Map;
 @SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+@Value("${spring.datasource.url}")
+private String dbUrl;
 
-  @Autowired
-  private DataSource dataSource;
+@Autowired
+private DataSource dataSource;
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
+public static void main(String[] args) throws Exception {
+  SpringApplication.run(Main.class, args);
+}
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
+@RequestMapping("/")
+String index() {
+  return "index";
+}
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+@RequestMapping("/hello")
+String hello(Map<String, Object> model) {
+model.put("message", "Welcome to my app!");
+return "hello";
+}
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
+@RequestMapping("/db")
+String db(Map<String, Object> model) {
+  try (Connection connection = dataSource.getConnection()) {
+    Statement stmt = connection.createStatement();
+    stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+    stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+    ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+    ArrayList<String> output = new ArrayList<String>();
+    while (rs.next()) {
+      output.add("Read from DB: " + rs.getTimestamp("tick"));
     }
-  }
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
+    model.put("records", output);
+    return "db";
+  } catch (Exception e) {
+    model.put("message", e.getMessage());
+    return "error";
   }
+}
+
+@Bean
+public DataSource dataSource() throws SQLException {
+  if (dbUrl == null || dbUrl.isEmpty()) {
+    return new HikariDataSource();
+  } else {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(dbUrl);
+    return new HikariDataSource(config);
+  }
+}
 
 }
