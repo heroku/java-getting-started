7aaf7e160c4721640e5c094a0b3036a695056b2e
septioktapiani <69469079+septioktapiani@users.noreply.github.com>
2020-08-13 12:29:08 +0700

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -73,6 +73,12 @@ public class Main {
       return "error";
     }
   }
+  
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+    model.put("message", "Welcome to my app!");
+    return "hello";
+  }
 
   @Bean
   public DataSource dataSource() throws SQLException {
