24f3484cbaebc254d5da8e9d139df0ed1d15c0f0
Phaniraj Nagalamadugu Srinivasulu <psrinivasulu@psriniv-ltm7627.internal.salesforce.com>
2021-05-07 12:10:32 +0530

    Updated Hello controller

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -84,10 +84,15 @@ public class Main {
   @RequestMapping("/hello")
   String hello(Map<String, Object> model) {
     RelativisticModel.select();
-    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-    model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+    String energy = System.getenv().get("ENERGY");
+    if (energy == null) {
+      energy = "12 GeV";
+    }
+    Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+    model.put("science", "E=mc^2: " + energy + " = "  + m.toString());
     return "hello";
   }
+
 
   @Bean
   public DataSource dataSource() throws SQLException {
