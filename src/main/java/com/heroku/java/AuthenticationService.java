8c6a95793c445c7076a7e4250be0f7c592476ef9
Priya Mukherjee <priyanjolim@gmail.com>
2025-07-31 13:09:59 -0700

    feat: added authentication service

---
+++ src/main/java/com/heroku/java/AuthenticationService.java
@@ -0,0 +1,32 @@
+package com.heroku.java;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final DataSource dataSource;
+
+    @Autowired
+    public AuthenticationService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public boolean checkAdminPrivilege(String password) {
+        Connection connection = dataSource.getConnection();
+        final var statement = connection.createStatement();
+        final var resultSet = statement.executeQuery("SELECT * FROM role WHERE permission = 'admin'");
+        
+        while (resultSet.next()) {
+            String pwd = resultSet.getString("password");
+            if (pwd.equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
